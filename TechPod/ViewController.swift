8d1aff52a5f4b170fb6faa70dfb538c41f3f93f7
Erepyon <erepyon@marinaharada.local>
2020-02-04 18:36:15 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by Erepyon on 2020/02/04.
+//  Copyright © 2020 Erepyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
