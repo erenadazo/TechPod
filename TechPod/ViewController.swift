9fff9d9b0cfdd8a36fe4323c434e36c7c5caaf97
Erena Harada <Elena@marinaharada.local>
2020-08-25 19:31:39 +0900

    あ

--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    //StoryBoardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
+    //曲名を入れるために配列
+    var songNameArray = [String]()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //テーブルビューのデータソースメソッドはViewControllクラスに書くよ、という設定
+        table.dataSource = self
+        
+        //songNameArrayに曲名を入れていく
+        
+        songNameArray = ["カノン", "エリーゼのために", "G線上のアリア"]
+        
     }
+    
+  
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       //セルの数をsongNameArrayの数にする
+        return songNameArray.count
+    }
+    
+    
+    
+    //ID付きのセルを取得して、　セル付属のtextLabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "Cell")
+        //セルにsongNameArrayの曲名を表示する
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    //セルが押された時に呼ばれるメソッド
+//    func tableView(_ tableView: UITableView, didSelect) {
+//        return
+//    }
+
 
 
 }
